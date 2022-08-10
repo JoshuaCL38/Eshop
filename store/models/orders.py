45a206545b68647008e0e14a3fd82637a6f1d057
Joshua LaForest <josh.laforest38@gmail.com>
2022-08-10 15:33:26 -0800

    first commit

--- store/models/orders.py
+++ store/models/orders.py
@@ -8,7 +8,7 @@ class Order(models.Model):
     product = models.ForeignKey(Products,
                                 on_delete=models.CASCADE)
     customer = models.ForeignKey(Customer,
-                                 on_delete=models.CASCADE)
+                                on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
     price = models.IntegerField()
     address = models.CharField (max_length=50, default='', blank=True)
--- store/models/product.py
+++ store/models/product.py
@@ -6,6 +6,7 @@ class Products(models.Model):
     category= models.ForeignKey(Category,on_delete=models.CASCADE,default=1 )
     description= models.CharField(max_length=250, default='', blank=True, null= True)
     image= models.ImageField(upload_to='uploads/products/')
+    items_available = models.IntegerField(default=0)
 
     @staticmethod
     def get_products_by_id(ids):
